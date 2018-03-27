db76f066cca35c070c252315416b0db6d764571d
philgrayphilgray <thephilgray@gmail.com>
2018-03-27 09:15:32 -0400

    Wrap card details and controls in card__body class to fix layout issue.

--- 000_album-collector/components/Card.vue
+++ 000_album-collector/components/Card.vue
@@ -1,29 +1,30 @@
 <template lang="pug">
   .card
     img.card__image(:src="album.art" :alt="'Album artwork for ' + album.title + ' by ' + album.artist")
-    .card__details
-      p {{album.title}}
-      p {{album.artist}}
-      p {{album.year}}
-      StarRating(:rating="album.rating")
-    .card__controls
-      button.card__button(type="button" aria-label="Edit card" @click="updateHandler(album.id)")
-        img.card__icon(src="~/assets/svg/pencil.svg" alt="Pencil")
-      button.card__button(type="button" aria-label="Delete card" @click="removeHandler(album.id)")
-        img.card__icon(src="~/assets/svg/bin.svg" alt="Bin")
+    .card__body
+      .card__details
+        p {{album.title}}
+        p {{album.artist}}
+        p {{album.year}}
+        StarRating(:rating="album.rating")
+      .card__controls
+        button.card__button(type="button" aria-label="Edit card" @click="updateHandler(album.id)")
+          img.card__icon(src="~/assets/svg/pencil.svg" alt="Pencil")
+        button.card__button(type="button" aria-label="Delete card" @click="removeHandler(album.id)")
+          img.card__icon(src="~/assets/svg/bin.svg" alt="Bin")
     
 </template>
 
 <script>
-import StarRating from "./StarRating";
+import StarRating from './StarRating';
 export default {
-  props: ["album"],
+  props: ['album'],
   components: {
     StarRating
   },
   methods: {
     removeHandler(id) {
-      this.$store.commit("remove", id);
+      this.$store.commit('remove', id);
     },
     updateHandler(id) {
       this.$router.push({ path: `/${id}/edit` });
@@ -34,15 +35,22 @@ export default {
 
 <style lang="scss">
 .card {
-  display: flex;
-  flex-wrap: wrap;
+  position: relative;
   width: 310px;
   padding: 1em;
   box-shadow: 1px 4px 2px 1px #aaa;
 }
 
+.card__body {
+  display: flex;
+  flex-wrap: wrap;
+}
+
 .card__image {
   flex: 100%;
+  width: 100%;
+  height: auto;
+  position: relative;
 }
 
 .card__details {
